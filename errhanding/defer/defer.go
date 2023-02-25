a896b85d552373faae55102308afa187c0118bd8
王逢超 <wangfengchao@nextmar.com>
2023-02-25 18:49:14 +0800

    阶段一学习完成

---
+++ errhanding/defer/defer.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"learnGo/functional/fib"
+	"os"
+)
+
+func tryDefer() {
+	defer fmt.Println(1)
+	defer fmt.Println(2)
+	fmt.Println(3)
+	// panic("error occurred")
+	fmt.Println(4)
+}
+
+func writeFile(filename string) {
+	file, err := os.OpenFile(filename, os.O_EXCL|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println("file already exists")
+		return
+	}
+	defer file.Close()
+	writer := bufio.NewWriter(file)
+	defer writer.Flush()
+
+	f := fib.Fibonacci()
+	for i := 0; i < 20; i++ {
+		fmt.Fprintln(writer, f())
+	}
+}
+
+func main() {
+	tryDefer()
+	writeFile("fib.txt")
+	var x = []int{1, 5: 4, 6}
+	fmt.Println(x)
+}
---
+++ test/test0/main.go
@@ -0,0 +1,9 @@
+package main
+
+// 12. 下列定义，会报错的是：A (逢超
+// A. var x = []int{1, 5: 4, 6}
+// B. type person struct{ name string }
+// C. ages := make(map[int][]string, 10, 5)
+// D. var i interface{}
+
+func main() {}
