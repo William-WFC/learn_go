fbea27859126aa84b2d04c91b6a2d74c31870059
王逢超 <wangfengchao@nextmar.com>
2023-02-26 16:46:19 +0800

    add

--- errhanding/defer/defer.go
+++ errhanding/defer/defer.go
@@ -15,10 +15,21 @@ func tryDefer() {
 	fmt.Println(4)
 }
 
+func Error(err error) string {
+	return "custom err"
+}
+
 func writeFile(filename string) {
 	file, err := os.OpenFile(filename, os.O_EXCL|os.O_CREATE, 0666)
+	// err = errors.New("this is a custom err")
 	if err != nil {
-		fmt.Println("file already exists")
+		// fmt.Println(err.Error())
+		fmt.Println(Error(err))
+		// if pathError, ok := err.(*os.PathError); !ok {
+		// 	panic(err)
+		// } else {
+		// 	fmt.Printf("%s, %s, %s\n", pathError.Op, pathError.Path, pathError.Err)
+		// }
 		return
 	}
 	defer file.Close()
